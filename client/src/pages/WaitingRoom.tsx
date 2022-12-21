df0d7b3b442bbfff5f9dafde96486a5d1549365c
fatihes1 <es.fatih3316@gmail.com>
2022-12-21 13:02:32 +0300

    handled nomination | created socket event | kicked someone from poll

--- client/src/pages/WaitingRoom.tsx
+++ client/src/pages/WaitingRoom.tsx
@@ -1,14 +1,134 @@
 import React, { useEffect } from 'react';
-import { actions } from '../state';
+import {actions, state} from '../state';
+import {useCopyToClipboard} from "react-use";
+import {useSnapshot} from "valtio";
+import {BsPencilSquare, MdContentCopy, MdPeopleOutline} from "react-icons/all";
+import {colorizeText} from "../util";
+import ConfirmationDialog from "../components/ui/ConfirmationDialog";
+import ParticipantList from "../components/ParticipantList";
+import NominationForm from "../components/NominationForm";
 
 export const WaitingRoom: React.FC = () => {
+    const [_copiedText, copyToClipboard] = useCopyToClipboard();
+    const [isParticipantListOpen, setIsParticipantListOpen] = React.useState(false);
+    const [isNominationFromOpen, setIsNominationFormOpen] = React.useState(false);
+    const [isConfirmationOpen, setIsConfirmationOpen] = React.useState(false);
+    const [confirmationMessage, setConfirmationMessage] = React.useState('');
+    const [participantToRemove, setParticipantToRemove] = React.useState<string>();
+    const [showConfirmation, setShowConfirmation] = React.useState(false);
+
+    const currentState = useSnapshot(state);
+
+    const confirmRemoveParticipant = (id: string) => {
+        setConfirmationMessage(`Remove ${currentState.poll?.participants[id]} from the poll?`);
+        setParticipantToRemove(id);
+        setIsConfirmationOpen(true);
+    }
+
+    const submitRemoveParticipant = () => {
+        participantToRemove && actions.removeParticipant(participantToRemove);
+        setIsConfirmationOpen(false);
+    }
+
     useEffect(() => {
         console.log('Waiting room useEffect');
         actions.initializeSocket();
     }, []);
     return (
-        <div className="flex flex-col w-full justify-between items-center h-full">
-            <h3>Waiting Room</h3>
-        </div>
+        <>
+            <div className="flex flex-col w-full justify-between items-center h-full">
+                <div>
+                    <h2 className="text-center">Poll Topic</h2>
+                    <p className="italic text-center mb-4">{ currentState.poll?.topic || '' }</p>
+                    <h2 className="text-center">Poll ID</h2>
+                    <h3 className="text-center mb-2">Click to copy!</h3>
+                    <div
+                        onClick={() => copyToClipboard(currentState.poll?.id || '')}
+                        className="mb-4 flex justify-center align-middle cursor-pointer"
+                    >
+                        <div className="font-extrabold text-center mr-2">
+                            {currentState.poll && colorizeText(currentState.poll.id)}
+                        </div>
+                        <MdContentCopy size={24} />
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <button
+                        className="box btn-orange mx-2 pulsate"
+                        onClick={() => setIsParticipantListOpen(true)}
+                    >
+                        <MdPeopleOutline size={24} />
+                        <span>{currentState.participantCount}</span>
+                    </button>
+                    <button
+                        className="box btn-purple mx-2 pulsate"
+                        onClick={() => setIsNominationFormOpen(true)}
+                    >
+                        <BsPencilSquare size={24} />
+                        <span>{currentState.nominationCount}</span>
+                    </button>
+                </div>
+                <div className="flex flex-col justify-center">
+                    {currentState.isAdmin ? (
+                      <>
+                          <div className="my-2 italic">
+                              {currentState.poll?.votesPerVoter} Nominations Required to Start !
+                          </div>
+                          <button
+                            className="box btn-orange my-2"
+                            disabled={!currentState.canStartVote}
+                            onClick={() => console.log('Will add start vote next time !')}
+                          >
+                              Start Voting
+                          </button>
+                      </>
+                    ) : (
+                      <div className="my-2 italic">
+                          Waiting for Admin, {' '}
+                          <span className="font-bold text-blue-700">
+                              {currentState.poll?.participants[currentState.poll?.adminID]}
+                          </span>
+                          , to start the voting !
+                      </div>
+                    )}
+                    <button
+                        className="box btn-purple my-2"
+                        onClick={() => setShowConfirmation(true)}
+                    >
+                        Leave Poll
+                    </button>
+                    <ConfirmationDialog
+                      message="You'll be kicked out of the poll"
+                      showDialog={showConfirmation}
+                      onCancel={() => setShowConfirmation(false)}
+                      onConfirm={() => actions.startOver()}
+                    />
+                </div>
+            </div>
+            <ParticipantList
+              isOpen={isParticipantListOpen}
+              onClose={() => setIsParticipantListOpen(false)}
+              participants={currentState.poll?.participants}
+              onRemoveParticipant={confirmRemoveParticipant}
+              isAdmin={currentState.isAdmin || false}
+              userID={currentState.me?.id}
+            />
+            <NominationForm
+                title={currentState.poll?.topic || ''}
+                isOpen={isNominationFromOpen}
+                onClose={() => setIsNominationFormOpen(false)}
+                onSubmitNomination={(nominationText) => actions.nominate(nominationText)}
+                nominations={currentState.poll?.nominations}
+                userID={currentState.me?.id}
+                onRemoveNomination={(nominationID) => actions.removeNomination(nominationID)}
+                isAdmin={currentState.isAdmin || false}
+            />
+            <ConfirmationDialog
+              message={confirmationMessage}
+              showDialog={isConfirmationOpen}
+              onCancel={() => setIsConfirmationOpen(false)}
+              onConfirm={() => submitRemoveParticipant() }
+            />
+        </>
     );
 };
--- client/src/state.ts
+++ client/src/state.ts
@@ -1,7 +1,7 @@
 import { Poll } from 'shared/poll-types';
 import { Socket } from 'socket.io-client';
 import { proxy, ref } from 'valtio';
-import { derive, subscribeKey } from 'valtio/utils';
+import { subscribeKey } from 'valtio/utils';
 import { createSocketWithHandlers, socketIOUrl } from './socket-io';
 import { getTokenPayload } from './util';
 import {nanoid} from "nanoid";
@@ -29,54 +29,57 @@ type WsErrorUnique = WsError & {
 
 export type AppState = {
     isLoading: boolean;
-    me?: Me;
     currentPage: AppPage;
     poll?: Poll;
     accessToken?: string;
     socket?: Socket;
     wsErrors: WsErrorUnique[];
+    me?: Me;
+    isAdmin: boolean;
+    nominationCount: number;
+    participantCount: number;
+    canStartVote: boolean;
 };
 
-const state: AppState = proxy({
+const state = proxy<AppState>({
     isLoading: false,
     currentPage: AppPage.Welcome,
     wsErrors: [],
+    get me() {
+        const accessToken = this.accessToken;
+
+        if (!accessToken) {
+            return;
+        }
+        const token = getTokenPayload(accessToken);
+        return {
+            id: token.sub,
+            name: token.name,
+        };
+    },
+    get isAdmin() {
+        if(!this.me) {
+            return false;
+        }
+        return this.me?.id === this.poll?.adminID;
+    },
+    get participantCount() {
+        return Object.keys(this.poll?.participants || {}).length;
+    },
+    get nominationCount() {
+        return Object.keys(this.poll?.nominations || {}).length;
+    },
+    get canStartVote() {
+        const votesPerVoter = this.poll?.votesPerVoter ?? 100;
+
+        return this.nominationCount >= votesPerVoter * this.participantCount;
+    }
 });
 
-const stateWithComputed: AppState = derive(
-    {
-        me: (get) => {
-            const accessToken = get(state).accessToken;
-
-            if (!accessToken) {
-                return;
-            }
-
-            const token = getTokenPayload(accessToken);
-
-            return {
-                id: token.sub,
-                name: token.name,
-            };
-        },
-        isAdmin: (get) => {
-            if (!get(state).me) {
-                return false;
-            }
-            return get(state).me?.id === get(state).poll?.adminID;
-        },
-    },
-    {
-        proxy: state,
-    }
-);
 
 const actions = {
     setPage: (page: AppPage): void => {
         state.currentPage = page;
-    },
-    startOver: (): void => {
-        actions.setPage(AppPage.Welcome);
     },
     startLoading: (): void => {
         state.isLoading = true;
@@ -106,6 +109,31 @@ const actions = {
     updatePoll: (poll: Poll): void => {
         state.poll = poll;
     },
+    nominate: (text: string): void => {
+        state.socket?.emit('nominate', {text});
+    },
+    startOver: (): void => {
+      actions.reset();
+      localStorage.removeItem('accessToken');
+      actions.setPage(AppPage.Welcome);
+    },
+    reset: (): void => {
+      state.socket?.disconnect();
+      state.poll = undefined;
+      state.accessToken = undefined;
+      state.isLoading = false;
+      state.socket = undefined;
+      state.wsErrors = [];
+    },
+    removeNomination: (id: string): void => {
+      state.socket?.emit('remove_nomination', {id});
+    },
+    removeParticipant: (id: string): void => {
+      state.socket?.emit('remove_participant', {id});
+    },
+    startVote: (): void => {
+      state.socket?.emit('start_vote');
+    },
     addWsError: (error: WsError): void => {
         state.wsErrors = [
             ...state.wsErrors,
@@ -115,6 +143,7 @@ const actions = {
             },
         ];
     },
+
     removeWsError: (id: string): void => {
         state.wsErrors = state.wsErrors.filter((error) => error.id !== id);
     },
@@ -128,4 +157,4 @@ subscribeKey(state, 'accessToken', () => {
 
 export type AppActions = typeof actions;
 
-export { stateWithComputed as state, actions };
+export {  state, actions };
